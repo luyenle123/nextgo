531574aae01fdd0715be716fd36bdc1f1c8d9bec
KA GO <luyenlv35@gmail.com>
2024-09-06 15:17:44 +0700

    fix build error

--- src/app/demo/fetchdata/page.tsx
+++ src/app/demo/fetchdata/page.tsx
@@ -1,17 +1,14 @@
 
 export default async function fetchdata(){
 
-    const data = await fetch('https://api.vercel.app/blog')
-    const posts = await data.json()    
-
+  const data = await fetch('https://api.vercel.app/blog')
+  const posts = await data.json()
   return (
-    <>
-        <ul>
-        {posts.map((post) => (
-            <li key={post.id}>{post.title}</li>
-        ))}
-        </ul>    
-    </>
+    <ul>
+      {posts.map((post) => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
   )
 
 }
