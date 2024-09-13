a247947ff57a254f73e73717017c91edc847b1f9
KA GO <luyenlv35@gmail.com>
2024-09-13 20:26:57 +0700

    update footer

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -10,9 +10,9 @@ export default function Footer(){
   return (
     <footer className="text-center h-40 inline-block w-full">
         <div className='flex w-full'>
-          <div className='w-1/2 h-36 mx-1 bg-slate-200'>
+          <div className='w-1/2 h-36 mx-1 bg-slate-200 hidden sm:block'>
           </div>
-          <div className='w-1/2 h-36 mx-1 bg-slate-200'>
+          <div className='w-full sm:w-1/2 h-36 mx-1 bg-slate-200'>
             <div className='flex float-right mt-3 mr-3'>
               <Image src={visa} width={50} height={30} alt='visa' className='mr-1'></Image>
               <Image src={amex} width={50} height={30} alt='visa' className='mr-1'></Image>
